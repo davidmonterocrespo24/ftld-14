6f83fc9b0dcd9792213d6109a7d21608b0ffa083
dmontero <dmontero@quanam.com>
2021-08-21 17:24:46 -0300

    fix

--- product_bundle_pack/models/product.py
+++ product_bundle_pack/models/product.py
@@ -14,7 +14,7 @@ class ProductPack(models.Model):
     product_id = fields.Many2one(comodel_name='product.product', string='Product', required=True)
     qty_uom = fields.Float(string='Quantity', required=True, defaults=1.0)
     bi_product_template = fields.Many2one(comodel_name='product.template', string='Product pack')
-    bi_image = fields.Binary(related='product_id.image_medium', string='Image', store=True)
+    bi_image = fields.Binary(related='product_id.image_1024', string='Image', store=True)
     price = fields.Float(related='product_id.lst_price', string='Product Price')
     uom_id = fields.Many2one(related='product_id.uom_id' , string="Unit of Measure", readonly="1")
     name = fields.Char(related='product_id.name', readonly="1")
