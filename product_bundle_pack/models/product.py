c6ebc2a035b8ed61d5b3d47424041742c7cd4fa7
David Montero Crespo <davidmonterocrespo24@gmail.com>
2021-09-04 14:09:13 -0300

    Update product.py

--- product_bundle_pack/models/product.py
+++ product_bundle_pack/models/product.py
@@ -13,7 +13,7 @@ class ProductPack(models.Model):
 	_description = "Product Pack"
 	
 	product_id = fields.Many2one(comodel_name='product.product', string='Product', required=True)
-	qty_uom = fields.Float(string='Quantity', required=True, defaults=1.0)
+	qty_uom = fields.Float(string='Quantity', required=True, default=1.0)
 	bi_product_template = fields.Many2one(comodel_name='product.template', string='Product pack')
 	bi_image = fields.Binary(related='product_id.image_1920', string='Image', store=True)
 	price = fields.Float(related='product_id.lst_price', string='Product Price')
