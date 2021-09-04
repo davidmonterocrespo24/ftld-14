e2fc86895319c542b72038e7929432459abc1d37
David Montero Crespo <davidmonterocrespo24@gmail.com>
2021-09-04 17:27:03 -0300

    Update product_bundle_wizard.py

--- product_bundle_pack/wizard/product_bundle_wizard.py
+++ product_bundle_pack/wizard/product_bundle_wizard.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
 
 from odoo import api, fields, models, _
 from datetime import datetime, timedelta, date
