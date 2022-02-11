7fb42597cdfd5e148d9b1655fa6aa8efda56f152
dmontero <dmontero@quanam.com>
2022-02-11 10:41:00 -0300

    mensaje

--- woo_price_list/models/woo_process_import_export.py
+++ woo_price_list/models/woo_process_import_export.py
@@ -40,98 +40,183 @@ class WooProcessImportExport(models.TransientModel):
         name = ""
         sku = False
         for woo_p in woo_products_ids:
-            item_ids = self.env['product.pricelist.item'].search([('product_tmpl_id', '=', woo_p.product_tmpl_id.id)],
-                                                                 order="min_quantity")
-            data = """
-                        {
-                          "type": "package",
-                          "filters": [
-                               {
-                                    "qty": 1,
-                                    "type": "product_sku",
-                                    "method": "in_list",
-                                    "value": [
-                                         "producto-price-list"
-                                    ],
-                                    "product_exclude": {
-                                         "on_wc_sale": "",
-                                         "already_affected": "",
-                                         "backorder": "",
-                                         "values": []
-                                    }
-                               }
-                          ],
-                          "title": "Producto price list",
-                          "priority": "",
-                          "enabled": "on",
-                          "sortable_blocks_priority": [
-                               "roles",
-                               "bulk-adjustments"
-                          ],
-                          "additional": {
-                               "conditions_relationship": "and",
-                               "is_replaced": false,
-                               "replace_name": "",
-                               "is_replace_free_products_with_discount": false,
-                               "free_products_replace_name": "",
-                               "sortable_apply_mode": "consistently"
-                          },
-                          "conditions": [],
-                          "cart_adjustments": [],
-                          "limits": [],
-                          "bulk_adjustments": {
-                               "type": "bulk",
-                               "qty_based": "not",
-                               "discount_type": "price__fixed",
-                               "ranges": [
-                                    
-                               ],
-                               "table_message": ""
-                          },
-                          "role_discounts": [],
-                          "get_products": {
-                               "repeat": "-1",
-                               "repeat_subtotal": ""
-                          },
-                          "options": {
-                               "apply_to": "expensive",
-                               "repeat": -1
-                          },
-                          "advertising": {
-                               "discount_message": "",
-                               "long_discount_message": "",
-                               "sale_badge": ""
-                          }
-                     }
-                        """
+            if woo_p.product_tmpl_id.default_code:
+                item_ids = self.env['product.pricelist.item'].search([('product_tmpl_id', '=', woo_p.product_tmpl_id.id)],
+                                                                     order="min_quantity")
+                data = """
+                            {
+                              "type": "package",
+                              "filters": [
+                                   {
+                                        "qty": 1,
+                                        "type": "product_sku",
+                                        "method": "in_list",
+                                        "value": [
+                                             "producto-price-list"
+                                        ],
+                                        "product_exclude": {
+                                             "on_wc_sale": "",
+                                             "already_affected": "",
+                                             "backorder": "",
+                                             "values": []
+                                        }
+                                   }
+                              ],
+                              "title": "Producto price list",
+                              "priority": "",
+                              "enabled": "on",
+                              "sortable_blocks_priority": [
+                                   "roles",
+                                   "bulk-adjustments"
+                              ],
+                              "additional": {
+                                   "conditions_relationship": "and",
+                                   "is_replaced": false,
+                                   "replace_name": "",
+                                   "is_replace_free_products_with_discount": false,
+                                   "free_products_replace_name": "",
+                                   "sortable_apply_mode": "consistently"
+                              },
+                              "conditions": [],
+                              "cart_adjustments": [],
+                              "limits": [],
+                              "bulk_adjustments": {
+                                   "type": "bulk",
+                                   "qty_based": "not",
+                                   "discount_type": "price__fixed",
+                                   "ranges": [
+                                        
+                                   ],
+                                   "table_message": ""
+                              },
+                              "role_discounts": [],
+                              "get_products": {
+                                   "repeat": "-1",
+                                   "repeat_subtotal": ""
+                              },
+                              "options": {
+                                   "apply_to": "expensive",
+                                   "repeat": -1
+                              },
+                              "advertising": {
+                                   "discount_message": "",
+                                   "long_discount_message": "",
+                                   "sale_badge": ""
+                              }
+                         }
+                            """
 
-            # for r in pricelist.item_ids:
-            #     sku=False
-            #     if r.applied_on == '1_product':
-            #         name=r.name+" "+str(r.min_quantity)+" "+str(r.price)
-            #         woo_product_obj = self.env['woo.product.template.ept'].search([('product_tmpl_id', '=', r.product_tmpl_id.id)])
-            #         sku= r.product_tmpl_id.default_code
-            #     elif r.applied_on == '0_product_variant':
-            #         woo_product_obj = self.env['woo.product.product.ept'].search([('product_id', '=', r.product_id.id)])
-            #         name=r.product_id.display_name+" "+str(r.min_quantity)+ " "+str(r.price)
-            #         sku= r.product_id.default_code
-            #     if sku and woo_product_obj:
+                # for r in pricelist.item_ids:
+                #     sku=False
+                #     if r.applied_on == '1_product':
+                #         name=r.name+" "+str(r.min_quantity)+" "+str(r.price)
+                #         woo_product_obj = self.env['woo.product.template.ept'].search([('product_tmpl_id', '=', r.product_tmpl_id.id)])
+                #         sku= r.product_tmpl_id.default_code
+                #     elif r.applied_on == '0_product_variant':
+                #         woo_product_obj = self.env['woo.product.product.ept'].search([('product_id', '=', r.product_id.id)])
+                #         name=r.product_id.display_name+" "+str(r.min_quantity)+ " "+str(r.price)
+                #         sku= r.product_id.default_code
+                #     if sku and woo_product_obj:
 
-            res = json.loads(data)
-            f = 1
-            for i_id in item_ids:
-                if i_id.min_quantity == 0 and i_id.fixed_price == 0:
-                    continue
-                _logger.error(str(i_id.name))
-                res["bulk_adjustments"]["ranges"].append(
-                    {'from': f, 'to': i_id.min_quantity, 'value': i_id.fixed_price})
-                f = i_id.min_quantity + 1
+                res = json.loads(data)
+                f = 1
+                for i_id in item_ids:
+                    if i_id.min_quantity == 0 and i_id.fixed_price == 0:
+                        continue
+                    _logger.error(str(i_id.name))
+                    res["bulk_adjustments"]["ranges"].append(
+                        {'from': f, 'to': i_id.min_quantity, 'value': i_id.fixed_price})
+                    f = i_id.min_quantity + 1
 
-            res['filters'][0]['value'][0] = woo_p.product_tmpl_id.default_code
-            res["title"] = woo_p.display_name
-            # res["bulk_adjustments"]["ranges"].extend(ranges)
-            # res=json.dumps(res, indent=4, sort_keys=True)
-            json_final.append(res)
+                res['filters'][0]['value'][0] = woo_p.product_tmpl_id.default_code
+                res["title"] = woo_p.display_name
+                # res["bulk_adjustments"]["ranges"].extend(ranges)
+                # res=json.dumps(res, indent=4, sort_keys=True)
+                json_final.append(res)
+            else:
+
+                for woo_pv_id in woo_products_ids:
+                    item_ids = self.env['product.pricelist.item'].search([('product_id', '=', woo_pv_id.product_id.id)],
+                                                                         order="min_quantity")
+                    data = """
+                            {
+                              "type": "package",
+                              "filters": [
+                                   {
+                                        "qty": 1,
+                                        "type": "product_sku",
+                                        "method": "in_list",
+                                        "value": [
+                                             "producto-price-list"
+                                        ],
+                                        "product_exclude": {
+                                             "on_wc_sale": "",
+                                             "already_affected": "",
+                                             "backorder": "",
+                                             "values": []
+                                        }
+                                   }
+                              ],
+                              "title": "Producto price list",
+                              "priority": "",
+                              "enabled": "on",
+                              "sortable_blocks_priority": [
+                                   "roles",
+                                   "bulk-adjustments"
+                              ],
+                              "additional": {
+                                   "conditions_relationship": "and",
+                                   "is_replaced": false,
+                                   "replace_name": "",
+                                   "is_replace_free_products_with_discount": false,
+                                   "free_products_replace_name": "",
+                                   "sortable_apply_mode": "consistently"
+                              },
+                              "conditions": [],
+                              "cart_adjustments": [],
+                              "limits": [],
+                              "bulk_adjustments": {
+                                   "type": "bulk",
+                                   "qty_based": "not",
+                                   "discount_type": "price__fixed",
+                                   "ranges": [
+                                        
+                                   ],
+                                   "table_message": ""
+                              },
+                              "role_discounts": [],
+                              "get_products": {
+                                   "repeat": "-1",
+                                   "repeat_subtotal": ""
+                              },
+                              "options": {
+                                   "apply_to": "expensive",
+                                   "repeat": -1
+                              },
+                              "advertising": {
+                                   "discount_message": "",
+                                   "long_discount_message": "",
+                                   "sale_badge": ""
+                              }
+                         }
+                            """
+                    res = json.loads(data)
+                    f = 1
+                    for i_id in item_ids:
+                        if i_id.min_quantity == 0 and i_id.fixed_price == 0:
+                            continue
+                        _logger.error(str(i_id.name))
+                        res["bulk_adjustments"]["ranges"].append(
+                            {'from': f, 'to': i_id.min_quantity, 'value': i_id.fixed_price})
+                        f = i_id.min_quantity + 1
+
+                    res['filters'][0]['value'][0] = woo_pv_id.product_id.default_code
+                    res["title"] = woo_pv_id.display_name
+                    json_final.append(res)
+
+
+
+
 
         self.json_price_list = json_final
         _logger.error(str(json_final))
